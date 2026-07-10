7a6453bf1ac058b5fcd91b85245b10f91883c51b
CodeeCoffee <try.debug.mode@proton.me>
2026-07-09 22:34:25 -0700

    TASK: implementing user routes

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  public getUsers() {
+    return 'You sent a get request to Users';
+  }
+  @Get('/:id/{:optional}')
+  public getUserById(@Param('id') id: string) {
+    return 'You sent a get user by id to users';
+  }
+  @Post()
+  public createUsers(): string {
+    return 'You sent a new user';
+  }
+}
