8513e9dc02b4bc493a66286b5b237e068f3830bf
CodeeCoffee <try.debug.mode@proton.me>
2026-07-11 20:17:47 -0700

    TASK: user controller updated

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,22 +1,40 @@
-import { Controller, Get, Param, Query, Post, Body, Req, ParseIntPipe, Ip, Headers } from '@nestjs/common';
-import type { Request } from 'express';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { GetUsersParamDto } from './dto/get-users-param.dto';
+import { PatchUserDTO } from './dto/patch-user.dto';
+
 @Controller('users')
 export class UsersController {
   @Get()
   public getUsers(
-      @Param('id', ParseIntPipe) id: number | undefined,
-      @Query('page', ParseIntPipe) optional?: number | undefined,
-      @Query('limit', ParseIntPipe) limit?: number,
-    ) 
-  {
+    @Param() getUserParamDto: GetUsersParamDto,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page?: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit?: number,
+  ) {
+    console.log(getUserParamDto);
+    console.log('limit', limit);
+    console.log('page', page);
     return 'You sent a get request to Users';
   }
 
   @Post()
-  public createUsers(@Req() request: Request, @Headers() headers: any, @Ip() ip:string): string {
-    console.log('request', request);
-    console.log('headers', headers);
-    console.log('ip', ip);
+  public createUsers(@Body() createUserDto: CreateUserDto): string {
+    console.log('body', createUserDto);
     return 'You sent a new user';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDTO) {
+    console.log('patchUserDto', patchUserDto);
   }
 }
